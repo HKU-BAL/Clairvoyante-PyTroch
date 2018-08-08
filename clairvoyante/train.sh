a472cd165f926b26c8d13cfa7ec39e972fd4e144
William Lam <zcabwhy@ucl.ac.uk>
2018-08-08 15:11:01 +0800

    Added relu he initialisation and changed train.sh

--- clairvoyante/train.sh
+++ clairvoyante/train.sh
@@ -3,5 +3,5 @@
 # Trains the Clairvoyante Pytorch v3 model as a daemon process.
 
 git pull
-nohup python train.py & 2> nohup.out
+nohup python train.py --bin_fn ../training/tensor.bin & 2> nohup.out
 echo $! > save_pid.txt
