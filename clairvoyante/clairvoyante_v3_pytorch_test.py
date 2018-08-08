f2adc3a0b8868bc15ceb7069a2473a21f708f076
William Lam <zcabwhy@ucl.ac.uk>
2018-08-08 17:21:37 +0800

    test script

---
+++ clairvoyante/clairvoyante_v3_pytorch_test.py
@@ -0,0 +1,49 @@
+import sys
+import os
+import time
+import argparse
+import param
+import logging
+import pickle
+import numpy as np
+from threading import Thread
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+import param
+import sys
+import torch.optim as optim
+
+def MSE(sigmoid, YPH):
+    mse = nn.MSELoss(reduction='sum')
+    loss = mse(sigmoid, YPH)
+    print(sigmoid)
+    print(YPH)
+    print("MSELoss: "+str(loss)+"\n")
+
+    return loss
+
+def CEL(logits, YPH):
+    log_softmax = nn.LogSoftmax(dim=1)
+    CrossEntropy = log_softmax(logits) * -YPH
+    print(CrossEntropy)
+    print(YPH)
+    loss = CrossEntropy.sum()
+    print("CELoss: "+str(loss)+"\n")
+
+    return loss
+
+def MSETest():
+    # Test 1
+    x = torch.tensor([[1, 1],[1, 1]])
+    y = torch.tensor([[0,0],[0,0]])
+
+    loss = MSE(x,y)
+    print("X: " + str([[1, 1],[1,1]]))
+    print("Y: " + str([[0,0],[0,0]]))
+    assert(loss == 4)
+    print("Success")
+
+
+if __name__ == "__main__":
+    MSETest()
