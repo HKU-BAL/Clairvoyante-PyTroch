621541d858b7654c2c84e2b5dc0aa85b2fc57246
William Lam <zcabwhy@ucl.ac.uk>
2018-08-10 14:01:57 +0800

    test

--- clairvoyante/save_weights.py
+++ clairvoyante/save_weights.py
@@ -8,13 +8,26 @@ import utils_v2 as utils
 import torch
 # import clairvoyante_v2 as cv
 import clairvoyante_v3 as cv
+import clairvoyante_v3_pytorch as cpt
 import tensorflow as tf
+import os
+from ast import literal_eval as make_tuple
 
 # Saves the weights and biases in fullv3-illumina-novoalign-hg001+hg002-hg38 into files.
 
 if __name__ == "__main__":
-    utils.SetupEnv()
-    m = cv.Clairvoyante()
-    m.init()
+    # utils.SetupEnv()
+    # m = cv.Clairvoyante()
+    # m.init()
+    #
+    # m.restoreParameters('../trainedModels/fullv3-illumina-novoalign-hg001+hg002-hg38/learningRate1e-3.epoch500')
 
-    m.restoreParameters('../trainedModels/fullv3-illumina-novoalign-hg001+hg002-hg38/learningRate1e-3.epoch500')
+    mp = cpt.Net()
+
+    for filename in os.listdir('../illumina_2_parameters/'):
+        par = np.loadtxt(filename)
+        name_list = filename[:-4].split('_')
+        par_name = name_list[0] + "_" + name_list[1]
+        dimension = make_tuple(name_list[2])
+        print(par_name)
+        print(dimension)
