f61ad3c9910eef0118a29a67a97b96a89c0cd7e2
William Lam <zcabwhy@ucl.ac.uk>
2018-08-14 10:22:58 +0800

    change train sh script

--- clairvoyante/train.sh
+++ clairvoyante/train.sh
@@ -3,5 +3,5 @@
 # Trains the Clairvoyante Pytorch v3 model as a daemon process.
 
 git pull
-nohup python train.py --bin_fn /nas7/yswong/trainingData/tensor_all.bin & 2> nohup.out
+nohup python train.py --bin_fn /nas7/yswong/trainingData/tensor_all.bin --ochk_prefix ../pytorchModels/trainAll/ & 2> nohup.out
 echo $! > save_pid.txt
