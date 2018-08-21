842517ff6d5386f4b223fb9485f4b7c763ee1d4c
William Lam <zcabwhy@ucl.ac.uk>
2018-08-21 16:03:46 +0800

    try transpose

--- clairvoyante/load_weights.py
+++ clairvoyante/load_weights.py
@@ -19,8 +19,8 @@ if __name__ == "__main__":
     mp = cpt.Net()
 
     # Gets each param in folder and load it into pytorch model.
-    for filename in os.listdir('../illumina_2_parameters/'):
-        par = np.loadtxt('../illumina_2_parameters/' + filename)
+    for filename in os.listdir('../illumina_2_parameters/txt/'):
+        par = np.loadtxt('../illumina_2_parameters/txt/' + filename)
 
         # Gets rid of .txt extension.
         name_list = filename[:-4].split('_')
@@ -48,7 +48,8 @@ if __name__ == "__main__":
             # print(W)
             # print(dimension)
             # print(par.reshape(dimension))
-            W = torch.from_numpy(par.reshape(dimension)).permute(3,2,0,1)
+            # W = torch.from_numpy(par.reshape(dimension)).permute(3,2,0,1)
+            W = torch.from_numpy(par.reshape(dimension)).transpose((3,2,0,1))
             # print(W)
             # print(W.shape)
             # break
@@ -89,4 +90,4 @@ if __name__ == "__main__":
         print(W)
         print("\n")
 
-    torch.save(mp.state_dict(), "../pytorchModels/illumina_2/illumina_2_parameters.txt")
+    torch.save(mp.state_dict(), "../pytorchModels/illumina_2_transpose/illumina_2_parameters.txt")
