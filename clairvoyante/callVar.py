da073b52e3e1dfbf08be4bf0ad7dabafb68a1d45
William Lam <zcabwhy@ucl.ac.uk>
2018-08-23 12:24:38 +0800

    put back parallelism

--- clairvoyante/callVar.py
+++ clairvoyante/callVar.py
@@ -44,11 +44,12 @@ def Run(args):
         param.NUM_THREADS = args.threads
     device = torch.device('cuda:0' if torch.cuda.is_available() else 'cpu')
     m = cpt.Net()
-    # if torch.cuda.device_count() > 1:
-    #     print("Let's use", torch.cuda.device_count(), "GPUs!")
-    #     # dim = 0 [30, xxx] -> [10, ...], [10, ...], [10, ...] on 3 GPUs
-    #     m = nn.DataParallel(m)
+    if torch.cuda.device_count() > 1:
+        print("Let's use", torch.cuda.device_count(), "GPUs!")
+        # dim = 0 [30, xxx] -> [10, ...], [10, ...], [10, ...] on 3 GPUs
+        m = nn.DataParallel(m)
     m.to(device)
+    m = m.module
 
     # m.init()
     #
--- clairvoyante/train.py
+++ clairvoyante/train.py
@@ -32,12 +32,12 @@ def Run(args):
     # m = cpt.Net()
     device = torch.device('cuda:0' if torch.cuda.is_available() else 'cpu')
     m = cpt.Net()
-    # if torch.cuda.device_count() > 1:
-    #     print("Let's use", torch.cuda.device_count(), "GPUs!")
+    if torch.cuda.device_count() > 1:
+        print("Let's use", torch.cuda.device_count(), "GPUs!")
         # dim = 0 [30, xxx] -> [10, ...], [10, ...], [10, ...] on 3 GPUs
-        #m = nn.DataParallel(m)
+        m = nn.DataParallel(m)
     m.to(device)
-    #m = m.module
+    m = m.module
     # m.init()
 
     if args.chkpnt_fn != None:
