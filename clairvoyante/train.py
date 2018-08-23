2cdab44724704f74bfe11b2e39b84709943abfe2
William Lam <zcabwhy@ucl.ac.uk>
2018-08-23 11:37:07 +0800

    train script parallel gpu

--- clairvoyante/train.py
+++ clairvoyante/train.py
@@ -31,6 +31,10 @@ def Run(args):
     # m = cpt.Net()
     device = torch.device('cuda:0' if torch.cuda.is_available() else 'cpu')
     m = cpt.Net()
+    if torch.cuda.device_count() > 1:
+        print("Let's use", torch.cuda.device_count(), "GPUs!")
+        # dim = 0 [30, xxx] -> [10, ...], [10, ...], [10, ...] on 3 GPUs
+        m = nn.DataParallel(m)
     m.to(device)
     # m.init()
 
