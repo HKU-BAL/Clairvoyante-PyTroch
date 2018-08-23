5a87b363c5c05e865c4624c5490da236475042ea
William Lam <zcabwhy@ucl.ac.uk>
2018-08-23 12:38:12 +0800

    change batch size

--- clairvoyante/param.py
+++ clairvoyante/param.py
@@ -8,8 +8,8 @@ matrixNum = 4               # Please change this value in the dataPrepScripts at
 bloscBlockSize = 500
 
 # Model hyperparameters
-# trainBatchSize = 10000
-trainBatchSize = 1000
+trainBatchSize = 10000
+# trainBatchSize = 1000
 predictBatchSize = 1000
 initialLearningRate = 0.001
 learningRateDecay = 0.1
