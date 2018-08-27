c7cf8da3e7f6dc52450d3a62d412e96be688f073
William Lam <zcabwhy@ucl.ac.uk>
2018-08-27 10:37:09 +0800

    change output folder

--- clairvoyante/train.sh
+++ clairvoyante/train.sh
@@ -3,5 +3,5 @@
 # Trains the Clairvoyante Pytorch v3 model as a daemon process.
 
 git pull
-nohup python train.py --bin_fn /nas7/yswong/trainingData/tensor_all.bin --ochk_prefix ../pytorchModels/trainAll3_PGPU/v3 & 2> nohup.out
+nohup python train.py --bin_fn /nas7/yswong/trainingData/tensor_all.bin --ochk_prefix ../pytorchModels/trainAll4_1000PGPU/v4 & 2> nohup.out
 echo $! > save_pid.txt
