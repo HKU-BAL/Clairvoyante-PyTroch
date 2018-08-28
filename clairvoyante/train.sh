084ffe7162c065027f71fb1951fcc358b20d1b6d
William Lam <zcabwhy@ucl.ac.uk>
2018-08-28 17:10:21 +0800

    changed bach script

--- clairvoyante/train.sh
+++ clairvoyante/train.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # Trains the Clairvoyante Pytorch v3 model as a daemon process.
+# --ochk_prefix ../pytorchModels/trainAll4_1000PGPU/v4
 
-git pull
-nohup python train.py --bin_fn /nas7/yswong/trainingData/tensor_all.bin --ochk_prefix ../pytorchModels/trainAll4_1000PGPU/v4 & 2> nohup.out
+nohup python train.py --bin_fn /nas7/yswong/trainingData/tensor_all.bin & 2> nohup.out
 echo $! > save_pid.txt
